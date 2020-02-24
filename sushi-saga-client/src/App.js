463fa1b7812150e8925776a6ca9f11e3cda852e1
Anna Dybas <a.m.dybas@gmail.com>
2020-02-24 17:44:03 -0500

    Done.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -6,12 +6,53 @@ import Table from './containers/Table';
 const API = "http://localhost:3000/sushis"
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      sushi: [],
+      eaten: [],
+      plates: 0,
+      moneyLeft: 100
+    }
+  }
+
+  componentDidMount(){
+    console.log("mounting!")
+    fetch(API).then(res => res.json()).then(data => {
+      this.setState({sushi: data})
+    })
+  }
+
+  renderFourSushi = () => {
+      return this.state.sushi.slice(this.state.plates, this.state.plates + 4)
+  }
+
+  sushiHandler = (sushi) => {
+    console.log("I was clicked")
+    if((this.state.moneyLeft-sushi.price) >= 0){
+      let currentMoney = this.state.moneyLeft - sushi.price
+      this.setState({
+        eaten: [...this.state.eaten, sushi],
+        moneyLeft: currentMoney
+      })
+    } else {
+      alert(" Can't keep eating if you have no monies 🤑")
+    }
+  }
+
+  getMoreSushi = () => {
+    console.log("I'm getting 4 more plates")
+    if (this.state.plates < 100){
+      let currentPlate = this.state.plates
+      this.setState({plates: 4 + currentPlate})
+    }
+  }
 
   render() {
     return (
       <div className="app">
-        <SushiContainer />
-        <Table />
+        <SushiContainer renderFourSushi={this.renderFourSushi()} sushiHandler={this.sushiHandler} gettingMoreSushi={this.getMoreSushi} eatenSushi={this.state.eaten}/>
+        <Table moneyLeft={this.state.moneyLeft} amountOfPlates={this.state.eaten}/>
       </div>
     );
   }
--- sushi-saga-client/src/containers/Table.js
+++ sushi-saga-client/src/containers/Table.js
@@ -7,21 +7,16 @@ const Table = (props) => {
       return <div className="empty-plate" style={{ top: -7 * index }}/>
     })
   }
-
+  
   return (
     <Fragment>
       <h1 className="remaining">
-        You have: ${ /* Give me how much money I have left */ } remaining!
+        You have: ${props.moneyLeft} remaining!
       </h1>
       <div className="table">
         <div className="stack">
           {
-            /* 
-               renderPlates takes an array 
-               and renders an empty plate
-               for every element in the array
-            */
-            renderPlates([])
+            renderPlates(props.amountOfPlates)
           }
         </div>
       </div>
